8654d27bf4e0c1861bc17b8d28ed8931c5eca5db
IMISSED <Damienweifen@gmail.com>
2021-07-10 15:49:32 -0700

    TPAPI 4.0
    They've added connector which is "Slider" support [TP Connector](https://www.touch-portal.com/api/index.php?section=connectors)

--- TouchPortal-API/TouchPortalAPI/__init__.py
+++ TouchPortal-API/TouchPortalAPI/__init__.py
@@ -14,6 +14,7 @@ class TYPES:
     onConnect = 'info'
     onAction = 'action'
     onListChange = 'listChange'
+    onConnectorChange = 'connectorChange'
     onShutdown = 'closePlugin'
     onBroadcast = 'broadcast'
     onSettingUpdate = 'settings'
