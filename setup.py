093f4447c386384a04bd2f087e3f2fa3c8f80a00
Max Paperno <max@wdg.us>
2021-07-18 03:39:19 -0400

    Update setup.py, LICENCE.txt, and add PyPi GitHub action. [cherry-picked from @KillerBOSS2019]

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-from setuptools import setup, find_packages
- 
+from setuptools import setup  #, find_packages
+
 classifiers = [
   'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
+  'Intended Audience :: Developers',
   'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+  'Operating System :: MacOS',
+  'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9'
 ]
- 
+
 setup(
   name='TouchPortal API',
-  version='1.3',
+  version='1.4',
   description='Touch Portal API for Python',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description=open('README.md').read(),
   long_description_content_type="text/markdown",
-  url='https://github.com/KillerBOSS2019/TouchPortal-API',  
+  url='https://github.com/KillerBOSS2019/TouchPortal-API',
   author='Damien',
   author_email='DamienWeiFen@gmail.com',
-  license='MIT', 
+  license='MIT',
   classifiers=classifiers,
-  keywords='TouchPortal, API, Plugin', 
-  packages=find_packages(),
+  keywords='TouchPortal, API, Plugin',
+  packages=['TouchPortalAPI'],
+  python_requires='>=3.8',
   install_requires=[
       'pyee',
       'requests'
-      ] 
+      ]
 )
