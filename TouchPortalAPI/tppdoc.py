3b0e7f61304f311a54ed0d3adb249a30966b19b5
IMISSED <Damienweifen@gmail.com>
2022-06-13 13:29:38 -0700

    added connector/slider doc
    - Added doc for connector
    - Added no required to have doc field in py entry (if there is doc field It will use that)
    - Modified help page for tppdoc command

--- TouchPortalAPI/tppdoc.py
+++ TouchPortalAPI/tppdoc.py
@@ -16,10 +16,16 @@ def generateTableContent(entry, entryFile):
     table_content = f"""
 # {entry['name'].replace(" ", "-")}
 
+"""
+    if entry.get('doc'):
+        table_content += f"""
 ![Downloads](https://img.shields.io/github/downloads/{entry['doc']['repository'].split(":")[0]}/{entry['doc']['repository'].split(":")[1]}/total)
 ![Forks](https://img.shields.io/github/forks/{entry['doc']['repository'].split(":")[0]}/{entry['doc']['repository'].split(":")[1]})
 ![Stars](https://img.shields.io/github/stars/{entry['doc']['repository'].split(":")[0]}/{entry['doc']['repository'].split(":")[1]})
 ![License](https://img.shields.io/github/license/{entry['doc']['repository'].split(":")[0]}/{entry['doc']['repository'].split(":")[1]})
+"""
+
+    table_content += f"""
 - [{entry['name']}](#{entry['name'].replace(" ", "-")})
   - [Description](#description)
 """
@@ -34,6 +40,10 @@ def generateTableContent(entry, entryFile):
         table_content += """
     - [Actions](#actions)"""
 
+    if "TP_PLUGIN_CONNECTORS" in dir(entryFile) and entryFile.TP_PLUGIN_CONNECTORS:
+        table_content += """
+    - [Slider](#Sliders)"""
+
     if "TP_PLUGIN_STATES" in dir(entryFile) and entryFile.TP_PLUGIN_STATES:
         table_content += """
     - [States](#states)"""
@@ -42,7 +52,7 @@ def generateTableContent(entry, entryFile):
         table_content += """
     - [Events](#events)"""
 
-    if "Install" in entry['doc'].keys() and entry['doc']['Install']:
+    if entry.get("doc") and "Install" in entry['doc'].keys() and entry['doc']['Install']:
         table_content += """
   - [Installation Guide](#installation)"""
         
@@ -63,11 +73,11 @@ def generateAction(entry):
         "</tr>\n"
 
     for action in entry.keys():
-        table += f"<tr valign='top'><td>{action}</td>" + \
-            f"<td>{entry[action]['doc']}</td>" + \
+        table += f"<tr valign='top'><td>{entry[action]['name']}</td>" + \
+            f"<td>{entry[action]['doc'] if entry[action].get('doc') else 'No document available'}</td>" + \
             f"<td>{entry[action]['format'].replace('$', '')}</td>"
 
-        table += "<td><ul start=0>\n"
+        table += "<td><ol start=1>\n"
 
         for data in entry[action]['data'].keys():
             table += f"<li>[{data}] Type: {entry[action]['data'][data]['type']} &nbsp; \n"
@@ -83,7 +93,7 @@ def generateAction(entry):
                 table += f" ({entry[action]['data'][data]['minValue']}-{entry[action]['data'][data]['maxValue']})"
             table += "</li>\n"
 
-        table += "</ul></td>\n"
+        table += "</ol></td>\n"
         table += f"<td align=center>{'Yes' if 'hasHoldFunctionality' in entry[action].keys() and entry[action]['hasHoldFunctionality'] else 'No'}</td>\n"
 
     table += "</table>\n"
@@ -91,6 +101,40 @@ def generateAction(entry):
     actionDoc += table
 
     return actionDoc
+
+def generateConnectors(entry):
+    connectorDoc = "\n## Sliders\n"
+    table = "<table>\n"
+    table += "<tr valign='buttom'>" + "<th>Slider Name</th>" + "<th>Description</th>" + "<th>Format</th>" + \
+        "<th nowrap>Data<br/><div align=left><sub>choices/default (in bold)</th>" + "</tr>\n"
+
+    for connor in entry.keys():
+        table += f"<tr valign='top'><td>{entry[connor]['name']}</td>" + \
+            f"<td>{entry[connor]['doc'] if entry[connor].get('doc') else 'No document available'}</td>" + \
+            f"<td>{entry[connor]['format'].replace('$', '')}</td>"
+
+        table += "<td><ol start=1>\n"
+
+        for data in entry[connor]['data'].keys():
+            table += f"<li>[{data}] Type: {entry[connor]['data'][data]['type']} &nbsp; \n"
+
+            if entry[connor]['data'][data]['type'] == "choice" and entry[connor]['data'][data]['valueChoices']:
+                table += f"<b>{entry[connor]['data'][data]['default']}</b> {entry[connor]['data'][data]['valueChoices']}"
+            elif "default" in entry[connor]['data'][data].keys() and entry[connor]['data'][data]['default'] != "":
+                table += f"<b>{entry[connor]['data'][data]['default']}</b>"
+            else:
+                table += "&lt;empty&gt;"
+
+            if entry[connor]['data'][data]['type'] == "number":
+                table += f" ({entry[connor]['data'][data]['minValue']}-{entry[connor]['data'][data]['maxValue']})"
+            table += "</li>\n"
+        
+        table += "</ol></td>\n"
+    table += "</table>\n"
+    connectorDoc += table
+    return connectorDoc
+    
+
 
 def generateSetting(entry):
     settingDoc = "\n## Settings Overview\n"
@@ -127,7 +171,7 @@ def generateSetting(entry):
     return settingDoc
 
 def generateState(entry, baseid):
-    stateDoc = "\n### States\n"
+    stateDoc = "\n## States\n"
 
     stateDoc += f" <b>Base Id:</b> {baseid}.\n\n"
     stateDoc += "| Id | Name | Description | DefaultValue |\n"
@@ -138,7 +182,7 @@ def generateState(entry, baseid):
     return stateDoc
 
 def generateEvent(entry, baseid):
-    eventDoc = "\n### Events\n\n"
+    eventDoc = "\n## Events\n\n"
     eventDoc += f"<b>Base Id:</b> {baseid}.\n\n"
     eventDoc += "<table>\n"
     eventDoc += "<tr valign='buttom'>" + "<th>Id</th>" + "<th>Name</th>" + "<th nowrap>Evaluated State Id</th>" + \
@@ -162,9 +206,9 @@ def main(docArg=None):
 
     parser.add_argument(
         "target", metavar='<target>', type=str,
-        help='A build script that contains some infomations about the plugin. ' +
-        'Using given infomation about the plugin, this script will automatically build entry.tp (if given file is .py) and it will build the distro ' +
-        'based on which operating system you\'re using.'
+        help='tppdoc is a documanentation generator for TouchPortal plugins. It uses py entry to generates a markdown file.' + \
+            'It can generate a table for the plugin settings, connectors, actions, state, and event. and It will also show' + \
+                ' data field example, It can show max length, min value, max value for the data field.'
     )
 
     opts = parser.parse_args(docArg)
@@ -185,7 +229,8 @@ def main(docArg=None):
     documentation += f"""
 
 # Description
-{entry.TP_PLUGIN_INFO['doc']['description']}
+{entry.TP_PLUGIN_INFO['doc']['description'] if entry.TP_PLUGIN_INFO.get("doc") and entry.TP_PLUGIN_INFO['doc'].get("description") and entry.TP_PLUGIN_INFO['doc']['description'] != "" else "No description."}\n\n
+This documentation generated for {entry.TP_PLUGIN_INFO['name']} V{entry.TP_PLUGIN_INFO['version']} with [Python TouchPortal SDK](https://github.com/KillerBOSS2019/TouchPortal-API).
     """
     setting = generateSetting(entry.TP_PLUGIN_SETTINGS)
     documentation += setting
@@ -194,18 +239,24 @@ def main(docArg=None):
     action = generateAction(entry.TP_PLUGIN_ACTIONS)
     documentation += action
 
+    connector = generateConnectors(entry.TP_PLUGIN_CONNECTORS)
+    documentation += connector
+
     state = generateState(entry.TP_PLUGIN_STATES, entry.TP_PLUGIN_INFO['id'])
     documentation += state
 
     event = generateEvent(entry.TP_PLUGIN_EVENTS, entry.TP_PLUGIN_INFO['id'])
     documentation += event
 
-    if entry.TP_PLUGIN_INFO['doc'].get('Install'):
+    if entry.TP_PLUGIN_INFO.get("doc") and entry.TP_PLUGIN_INFO['doc'].get('Install'):
         documentation += "\n# Installation\n"
         documentation += entry.TP_PLUGIN_INFO['doc']['Install']
 
     documentation += "\n# Bugs and Suggestion\n"
-    documentation += f"Open an [issue](https://github.com/{'/'.join(entry.TP_PLUGIN_INFO['doc']['repository'].split(':'))}/issues) or join offical [TouchPortal Discord](https://discord.gg/MgxQb8r) for support.\n\n"
+    try:
+        documentation += f"Open an [issue](https://github.com/{'/'.join(entry.TP_PLUGIN_INFO['doc']['repository'].split(':'))}/issues) or join offical [TouchPortal Discord](https://discord.gg/MgxQb8r) for support.\n\n"
+    except:
+        documentation += f"Open an issue on github or join offical [TouchPortal Discord](https://discord.gg/MgxQb8r) for support.\n\n"
 
     documentation += "\n# License\n"
     documentation += "This plugin is licensed under the [GPL 3.0 License] - see the [LICENSE](LICENSE) file for more information.\n\n"
@@ -215,10 +266,3 @@ def main(docArg=None):
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
